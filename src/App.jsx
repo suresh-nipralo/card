e95da9abb007b677a398d40023bb981ac52fd6aa
suresh-nipralo <suresh.j@nipralo.com>
2025-10-27 11:25:04 +0530

    all added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import HomePage from './pages/Home';
-// import TeamMemberPage from './pages/TeamMember';
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/:slug" element={<TeamMemberPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import TeamMemberPage from './pages/TeamMember';
+import HomePage from './pages/Home';
 
 function App() {
   return (
@@ -26,6 +12,7 @@ function App() {
         {/* Redirect root to default member */}
         {/* <Route path="/" element={<Navigate to="/:slug" replace />} /> */}
         <Route path="/:slug" element={<TeamMemberPage />} />
+        <Route path="/" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   );
